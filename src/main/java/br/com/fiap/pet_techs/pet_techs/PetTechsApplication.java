2adc85aebd054aab0b16ccf40207ec6817154099
Lucas <mrrealcaldeira@gmail.com>
2024-09-30 17:28:31 -0300

    feat: initial project

---
+++ src/main/java/br/com/fiap/pet_techs/pet_techs/PetTechsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pet_techs.pet_techs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTechsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTechsApplication.class, args);
+	}
+
+}
